cf23d40b423f09c3719f6e1afdbd138947ac7248
vishi293 <vishilatiyan@gmail.com>
2026-06-08 08:15:20 +0530

    Update Adultuser.java

--- week 1/OOPS/Adultuser.java
+++ week 1/OOPS/Adultuser.java
@@ -1,1 +1,25 @@
+public class AdultUser implements LibraryUser{
+int age;
+String bookType;
+@Override
+public void registerAccount (int age) {
+this. age=age;
+if (age>12) {
+System. out. printin("You have successfully registered under an Adult Account");
+}
+  else{
+System. out.println("Sorry, Age must be greater than 12 to register as an adult"");
+  }
+}
+@Override
+public void requestBook(String bookType) {
+this.bookType=bookType;
+if(bookType== "Fiction"') (
+System.out printIn("Book Issued successfully, please return the book within 7 day");
+}
+  else{
+System. out.printIn("Oops, you are allowed to take only adult Fiction books");
+}
+}
+}
 
