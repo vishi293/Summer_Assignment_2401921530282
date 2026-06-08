f982f9c7af403cb606257176d87529e89de647b3
vishi293 <vishilatiyan@gmail.com>
2026-06-08 08:42:22 +0530

    Update LibInterfacedemo.java

--- week 1/OOPS/LibInterfacedemo.java
+++ week 1/OOPS/LibInterfacedemo.java
@@ -1,1 +1,14 @@
-
+public class LibraryinterfaceDemo{
+  public stratic void main(String[]args){
+      KidUser kid=new KidUser();
+      kid.registeraccount(10);
+      kid.requestBook("kids");
+       kid.registeraccount(18);
+      kid.requestBook("Fiction");
+      AdultUser adult = new AdultUser();
+      kid.registeraccount(5);
+      kid.requestBook("kids");
+      kid.registeraccount(23);
+      kid.requestBook("Fiction");
+  }
+}
