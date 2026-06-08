da879af56297fd24533e2a86ae71375e76ce9e64
vishi293 <vishilatiyan@gmail.com>
2026-06-08 08:24:27 +0530

    Update KidUser.java

--- week 1/OOPS/KidUser.java
+++ week 1/OOPS/KidUser.java
@@ -1,1 +1,25 @@
-
+public class KidUser implements LibraryUser{
+  int age;
+  String bookType;
+  
+  @Override
+public void registerAccount (int age) {
+this. age=age;
+if (age<12) {
+System. out. printin("You have successfully registered under a Kid Account");
+}
+  else{
+System. out.println("Sorry, Age must be smaller than 12 to register for an kid account ");
+  }
+}
+@Override
+public void requestBook(String bookType) {
+this.bookType=bookType;
+if(bookType== "Fiction"') (
+System.out printIn("Book Issued successfully, please return the book within 7 day");
+}
+  else{
+System. out.printIn("Oops, you are allowed to take only adult Fiction books");
+}
+}
+}  
