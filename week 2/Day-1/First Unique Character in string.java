e817f47a9d719dfc39451a6ea29ba6c5db864607
vishi293 <vishilatiyan@gmail.com>
2026-06-09 07:27:04 +0530

    Update and rename First Unique Character in string to First Unique Character in string.java

---
+++ week 2/Day-1/First Unique Character in string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqChar(String s) 
+    {
+       LinkedHashMap<Character,Integer> map=new LinkedHashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            }
+        for(int i=0; i<s.length();i++){
+            if(map.get(s.charAt(i))==1){
+                return i;
+            }
+        }        
+        return -1;
+    }
+}
