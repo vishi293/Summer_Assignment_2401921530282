11e9514eaa6bb6a92b29b0160dfe9b222b68c31a
vishi293 <vishilatiyan@gmail.com>
2026-06-10 16:21:07 +0530

    Update LongestSubstringwithoutRepeat.java

--- week 2/Day-2/LongestSubstringwithoutRepeat.java
+++ week 2/Day-2/LongestSubstringwithoutRepeat.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap <Character,Integer> map =new HashMap<>();
+        int left=0;
+        int maxlen=0;
 
+        for(int right=0;right<s.length();right++){
+            char ch= s.charAt(right);
+
+            if(map.containsKey(ch)){
+                left=Math.max(left,map.get(ch)+1);
+            }
+            map.put(ch,right);
+            maxlen=Math.max(maxlen,right-left+1);
+        }
+        return maxlen;
+    }
+}
