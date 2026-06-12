86aecc119f00cee14e7005746c49caddb21ec829
vishi293 <vishilatiyan@gmail.com>
2026-06-12 08:25:16 +0530

    Update Generate Parenthesis.java

--- week 2/Day-4/Generate Parenthesis.java
+++ week 2/Day-4/Generate Parenthesis.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public void generate(int n,int l,int r,String s,List<String> ans){
+        if(r==n){
+            ans.add(s);
+            return;
+        }
+        if(l<n){
+            generate(n,l+1,r,s+"(",ans);
+        }
+        if(r<l){
+            generate(n,l,r+1,s+")",ans);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans=new ArrayList<>();
+        generate(n,0,0,"",ans);
+        return ans;
+    }
+}
