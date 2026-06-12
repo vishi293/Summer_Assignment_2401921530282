d92d0482e537655ebf8b9fb9785ec0f2a890ba69
vishi293 <vishilatiyan@gmail.com>
2026-06-12 08:25:54 +0530

    Update Reverse words in a string III.java

--- week 2/Day-4/Reverse words in a string III.java
+++ week 2/Day-4/Reverse words in a string III.java
@@ -1,1 +1,24 @@
+class Solution {
+    public String reverseWords(String s) {
+        char [] arr =s.toCharArray();
+        int start=0;
 
+        for(int end=0;end<=arr.length;end++){
+            if(end==arr.length || arr[end] == ' '){
+                reverse(arr,start,end-1);
+                start=end+1;
+            }
+        }
+        return new String(arr);
+    }
+    private void reverse(char[] arr,int left,int right){
+        while(left<right){
+            char temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+
+            left++;
+            right--;
+        }
+    }
+}
