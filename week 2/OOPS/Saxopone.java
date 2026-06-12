4c3ff03cdfeafd242e8c0422025d105d26e787b4
vishi293 <vishilatiyan@gmail.com>
2026-06-12 08:33:04 +0530

    Update Saxopone.java

--- week 2/OOPS/Saxopone.java
+++ week 2/OOPS/Saxopone.java
@@ -1,1 +1,10 @@
+package music.wind;
 
+import music.playable;
+public class Saxophone implements playable{
+
+    @Override
+    public void play(){
+        System.out.println("Playable Saxophone");
+    }
+}
