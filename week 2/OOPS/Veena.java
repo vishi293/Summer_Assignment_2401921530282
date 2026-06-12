167ab9869350e4bbe5f87fe83fc1bba064b77f58
vishi293 <vishilatiyan@gmail.com>
2026-06-12 08:34:58 +0530

    Update Veena.java

--- week 2/OOPS/Veena.java
+++ week 2/OOPS/Veena.java
@@ -1,1 +1,10 @@
+package music.string;
 
+import music.playable;
+public class Veena implements playable{
+
+     @Override
+    public void play(){
+        System.out.println("Playable Veena");
+    }
+}
