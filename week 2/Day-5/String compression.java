aae96e5f928acba960746764f8e9802d04bb79f0
vishi293 <vishilatiyan@gmail.com>
2026-06-13 10:17:39 +0530

    Update String compression.java

--- week 2/Day-5/String compression.java
+++ week 2/Day-5/String compression.java
@@ -1,1 +1,28 @@
+class Solution {
+    public int compress(char[] chars) {
+        int write =0;
+        int read = 0;
 
+        while (read < chars.length) {
+            char currentChar = chars[read];
+            int count = 0;
+
+            while (read < chars.length && chars[read] == currentChar) {
+                read++;
+                count++;
+            }
+
+            chars[write++] = currentChar;
+
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+
+        return write;
+    }
+}
+        
+    
