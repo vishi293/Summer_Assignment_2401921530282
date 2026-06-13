4a92bdb8259d7613fc8de77731d2437c6d39f03b
vishi293 <vishilatiyan@gmail.com>
2026-06-13 09:33:24 +0530

    Update Group anagrams.java

--- week 2/Day-5/Group anagrams.java
+++ week 2/Day-5/Group anagrams.java
@@ -1,1 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+     Map<String, List<String>> map = new HashMap<>();
 
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+
+        
+    }
+}
