a3deb13e2116be32e865fff91161bb22a14fe39d
vishi293 <vishilatiyan@gmail.com>
2026-06-16 08:52:37 +0530

    Update Middle of the Linked List.java

--- week 3/Day -1/Middle of the Linked List.java
+++ week 3/Day -1/Middle of the Linked List.java
@@ -1,1 +1,24 @@
 
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head==null|| head.next==null){
+            return head;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=null;
+
+        ListNode head1=head;
+        ListNode head2=slow;
+
+        return head2;
+    }
+
+}
