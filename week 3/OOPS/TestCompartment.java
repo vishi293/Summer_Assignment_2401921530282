d68b86c9db3f1e020c242e07b73c4b578a248eac
vishi293 <vishilatiyan@gmail.com>
2026-06-20 09:24:00 +0530

    Update TestCompartment.java

--- week 3/OOPS/TestCompartment.java
+++ week 3/OOPS/TestCompartment.java
@@ -1,1 +1,31 @@
 
+public class testCompartment {
+    public static void main(String[] args) {
+        Luggage lugg = new Luggage();
+
+        int[] compartments = new int[10];
+        for (int i = 0; i < compartments.length; i++) {
+            int num = (int)(Math.random()*4)+1;
+
+            switch (num) {
+                case 1:
+                    compartments[i] = new Firstclass();
+                    break;
+                    case 2:
+                        compartments[i]= new general();
+                        break;
+                        case 3:
+                            compartments[i]=new Ladiesclass();
+                            break;
+                            case 4:
+                                compartments[i]=new Luggage();
+            }
+            System.out.println("Railway Compartments: ");
+
+            for (int j = 0; j < compartments.length; j++) {
+                System.out.println("Compartment " + (i + 1) + ": "
+                        + compartments[i].notice());
+            }
+        }
+    }
+}
