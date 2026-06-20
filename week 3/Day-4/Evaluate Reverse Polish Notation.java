8a07fe6ccac41bd2716bde90cde1bdde5c1e838c
vishi293 <vishilatiyan@gmail.com>
2026-06-20 09:03:24 +0530

    Update Evaluate Reverse Polish Notation.java

--- week 3/Day-4/Evaluate Reverse Polish Notation.java
+++ week 3/Day-4/Evaluate Reverse Polish Notation.java
@@ -1,1 +1,38 @@
 
+
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack <Integer> st=new Stack<>();
+
+        for(String token:tokens){
+
+            switch(token){
+                case "+":
+                    st.push(st.pop() + st.pop());
+                    break;
+
+                case "-":
+                    int a1=st.pop();
+                    int a2=st.pop();
+                    st.push(a2-a1);
+                    break;
+
+                case "*":
+                    st.push(st.pop()*st.pop());
+                    break;
+
+                case "/":
+                    int b1=st.pop();
+                    int b2=st.pop();
+                    st.push(b2/b1);
+                    break;
+
+                default:
+                    st.push(Integer.parseInt(token));
+            }
+        }
+        return st.pop();
+    }
+}
+
