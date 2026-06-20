50d45d3a6fe006161c1ed8d9d720bb25e9926426
vishi293 <vishilatiyan@gmail.com>
2026-06-20 09:02:36 +0530

    Update Daily Temperature.java

--- week 3/Day-4/Daily Temperature.java
+++ week 3/Day-4/Daily Temperature.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        Stack <Integer> st= new Stack<>();
+        int n=temp.length;
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && temp[i]>temp[st.peek()]){
+                int idx=st.pop();
+                arr[idx]=i-idx;
+            }
+            st.push(i);
+        }
+        return arr;
+    }
+}
 
