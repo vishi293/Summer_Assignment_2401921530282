b550b8bf845710ec52c1481c68bfb729b2a19ec2
vishi293 <vishilatiyan@gmail.com>
2026-06-27 08:53:34 +0530

    Update Test.java

--- week 4/OOPS/Test.java
+++ week 4/OOPS/Test.java
@@ -1,1 +1,17 @@
 
+public interface test{
+    int square(int x);
+}
+
+class Arithmatic implements test{
+    public int square(int x){
+        return x*x;
+    }
+}
+
+public class toTestInt{
+   public static void main(String[] args) {
+        Arithmatic a = new Arithmatic();
+        System.out.println(a.square(7));
+    }
+}
