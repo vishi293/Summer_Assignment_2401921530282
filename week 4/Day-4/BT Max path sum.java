9100a8c91692c824152ea4ad8eef7eaf8ec78489
vishi293 <vishilatiyan@gmail.com>
2026-06-27 08:40:41 +0530

    Update BT Max path sum.java

--- week 4/Day-4/BT Max path sum.java
+++ week 4/Day-4/BT Max path sum.java
@@ -1,1 +1,21 @@
 
+class Solution {
+    int maxSum=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return maxSum;
+    }
+
+    private int dfs(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+
+        int left=Math.max(0,dfs(node.left));
+        int right=Math.max(0,dfs(node.right));
+
+        maxSum=Math.max(maxSum, node.val+left+right);
+
+        return node.val + Math.max(left,right);
+    }
+}
