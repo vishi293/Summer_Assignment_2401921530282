63054390af64fcdef674d2f74021109c9aaf2af1
vishi293 <vishilatiyan@gmail.com>
2026-06-27 08:38:15 +0530

    Update BTInorderTraversal.java

--- week 4/Day-2/BTInorderTraversal.java
+++ week 4/Day-2/BTInorderTraversal.java
@@ -1,1 +1,17 @@
 
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List <Integer> result = new ArrayList<>();
+        inorder(root,result);
+        return result;
+    }
+
+    public void inorder(TreeNode node,List<Integer> result){
+        if(node==null){
+            return;
+        }
+        inorder(node.left,result);
+        result.add(node.val);
+        inorder(node.right,result);
+    }
+}
