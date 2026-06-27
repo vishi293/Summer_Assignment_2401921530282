b60b0795860549df28f692dd41ee6694cb8871c5
vishi293 <vishilatiyan@gmail.com>
2026-06-27 08:41:31 +0530

    Update Diameter of BT.java

--- week 4/Day-4/Diameter of BT.java
+++ week 4/Day-4/Diameter of BT.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    int d=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return d;
+    }
+    private int height(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        int left=height(node.left);
+        int right=height(node.right);
+
+        d=Math.max(d,left+right);
+
+        return 1+ Math.max(left,right);
+    }
+}
