27cd4b3df0fa5ccd5d0b4ac26c1af33c8ddc5712
vishi293 <vishilatiyan@gmail.com>
2026-06-27 08:51:40 +0530

    Update Inner and outer.java

--- week 4/OOPS/Inner and outer.java
+++ week 4/OOPS/Inner and outer.java
@@ -1,1 +1,23 @@
+class Outer{
+
+    void display(){
+        System.out.println("display method of outer class");
+    }
+
+    class Inner{
+        void display(){
+            System.out.println("display method of inner class");
+        }
+    }
+}
+
+   public class Main{
+      public static void main(String[] args) {
+           Outer o = new Outer();
+           o.display();
+
+           Outer.Inner in=obj.new Inner();
+           in.display();
+       }
+   }
 
