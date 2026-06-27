e761655b9c5c316ec840b0a41edd3ac5072d4173
vishi293 <vishilatiyan@gmail.com>
2026-06-27 08:42:14 +0530

    Update Construct BT from preorder and inorder traversal.java

--- week 4/Day-5/Construct BT from preorder and inorder traversal.java
+++ week 4/Day-5/Construct BT from preorder and inorder traversal.java
@@ -1,1 +1,29 @@
 
+class Solution {
+    int preidx=0;
+    HashMap <Integer,Integer> map= new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        for(int i=0;i<inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        return helper(preorder,0,inorder.length-1);
+
+    }
+    private TreeNode helper(int [] preorder,int left,int right){
+
+        if(left>right){
+            return null;
+        }
+
+        int rootval=preorder[preidx++];
+        TreeNode root=new TreeNode(rootval);
+
+        int idx=map.get(rootval);
+
+        root.left=helper(preorder,left,idx-1);
+        root.right=helper(preorder,idx+1,right);
+
+        return root;
+    }
+}
