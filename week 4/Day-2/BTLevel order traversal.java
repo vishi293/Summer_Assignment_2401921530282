ef4735bf38da15347aba669f182a7994dfe32629
vishi293 <vishilatiyan@gmail.com>
2026-06-27 08:37:28 +0530

    Update BTLevel order traversal.java

--- week 4/Day-2/BTLevel order traversal.java
+++ week 4/Day-2/BTLevel order traversal.java
@@ -1,1 +1,32 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+
+        if(root==null){
+            return res;
+        }
+
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int s=q.size();
+            List<Integer> arr=new ArrayList<>();
+
+            for(int i=0;i<s;i++){
+                TreeNode node=q.poll();
+                arr.add(node.val);
+
+                if(node.left!=null){
+                    q.offer(node.left);
+                }
+                if(node.right!=null){
+                    q.offer(node.right);
+                }
+            }
+            res.add(arr);
+        }
+        return res;
+    }
+}
 
